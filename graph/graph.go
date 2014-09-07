293c8f8088e25ca5eb7d913650615a687462092d
Rene Zhang <rz99@cornell.edu>
2014-09-06 20:43:39 -0400

    Added more construction of graph

--- graph/graph.go
+++ graph/graph.go
@@ -1,27 +1,51 @@
 package graph
 
+// Type representing a graph
 type Graph struct {
 	nodes map[Id]*Node
 }
 
+// Type representing a node
 type Node struct {
 	person *Person
 	in     []*DiEdge
 	out    []*DiEdge
 }
 
+// Type representing a directed edge with a cost and a reason
 type DiEdge struct {
 	source *Node
 	sink   *Node
 	value  int
+	reason string
 }
 
+// Initializes a new graph
 func NewGraph() *Graph {
 	return &Graph{make(map[Id]*Node)}
 }
 
-func (graph *Graph) NewNode(person *Person, basesize int) {
+// Constructs a new node from a person
+func createNode(person *Person, basesize int) *Node {
 	var in []*DiEdge = make([]*DiEdge, 0, basesize)
 	var out []*DiEdge = make([]*DiEdge, 0, basesize)
-	graph.nodes[1] = &Node{person, in, out}
+	return &Node{person, in, out}
+}
+
+// Adds a newly constructed node to the graph from a person
+func (graph *Graph) NewPerson(person *Person, basesize int) {
+	graph.nodes[person.id] = createNode(person, basesize)
+}
+
+// Removes a node from the graph based on person
+// Only works if node exists
+func (graph *Graph) RemovePerson(person *Person) {
+	_, ok := graph.nodes[person.id]
+	if ok {
+		delete(graph.nodes, person.id)
+	}
+}
+
+func (graph *Graph) AddEdge(source *Person, sink *Person, value int, reason string) {
+
 }
--- graph/person.go
+++ graph/person.go
@@ -1,19 +1,29 @@
 package graph
 
+// Type of an id
 type Id int
 
+// Type representing a person
 type Person struct {
 	id   Id
-	name string
+	Name string
 }
 
+// The next available id
 var curId Id = 0
 
+// Returns the next id
 func nextId() Id {
 	curId += 1
 	return curId - 1
 }
 
-func newPerson(name string) *Person {
+// Resets Ids to 0
+func ClearIds() {
+	curId = 0
+}
+
+// Constructs a new person with the next available id
+func NewPerson(name string) *Person {
 	return &Person{nextId(), name}
 }
