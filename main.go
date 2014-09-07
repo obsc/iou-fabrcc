88307eb0d646b603912319fa9ad9c80fd3068bb7
Rene Zhang <rz99@cornell.edu>
2014-09-07 13:53:05 -0400

    hello world website

--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
 )
 
 func main() {
-	fmt.Println("hi")
+	http.HandleFunc("/", hello)
+	fmt.Println("listening...")
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		panic(err)
+	}
+}
+func hello(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(res, "hello world!")
 }
