04445683d346e3809920d80312c630f47d356977
Bo Yuan Zhou <bz88@cornell.edu>
2014-09-11 22:56:24 -0400

    added posting functionality for transactions

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -1,4 +1,4 @@
-var users, graph, history;
+var userMap, graph, history;
 var rowsShown = 0;
 
 function listTransactions(transactions) {
@@ -17,13 +17,29 @@ function listTransactions(transactions) {
 	}
 }
 
+function mapSetup(users) {
+	userMap = {};
+	users.forEach( function(user) {
+		userMap[user.Name] = user.UserId;
+	});
+
+	// Setup autocomplete
+	var names = Object.keys(userMap);
+	$('#source').autocomplete({
+		source: names
+	});
+	$('#sink').autocomplete({
+		source: names
+	});
+}
+
 // Login Menu functions
 function login() {
 
 	$("#login").fadeTo(300, 0);
 
-	$.get("/users/json", null, function (data) { users = JSON.parse(data); });
-	$.get("/transactions/json", null, function (data) { transactions = JSON.parse(data); });
+	$.get("/users/json", null, function (data) { mapSetup(JSON.parse(data)); });
+	$.get("/transactions/json", null, function (data) { transactions = JSON.parse(data); console.log(transactions); });
 	$.get("/graph/json", null, function (data) { graph = JSON.parse(data); });
 
 	setTimeout(showContent, 300);
@@ -39,7 +55,7 @@ function register() {
 	};
 	$.post("/users/new", user, function (data) {console.log(data);});
 
-	$.get("/users/json", null, function (data) { users = JSON.parse(data); });
+	$.get("/users/json", null, function (data) { mapSetup(JSON.parse(data)); });
 	$.get("/transactions/json", null, function (data) { transactions = JSON.parse(data); });
 	$.get("/graph/json", null, function (data) { graph = JSON.parse(data); });
 
@@ -55,18 +71,19 @@ function expand() {
 function showContent() {
 	$('#user').toggleClass('hidden');
 	$('#content').toggleClass('hidden');
+	
+	// Content-related changes
 	$('#expand').click(expand);
 }
 
 function newTransaction() {
 
 	transaction = {
-		source : sourceId,
-		sink : sinkId,
-		value : value,
-		reason : reason
+		source : userMap[$('#source').val()],
+		sink : userMap[$('#sink').val()],
+		value : +($('#amount').val()),
+		reason : $('#reason').val()
 	};
-
-	$.post("transactions/new", function (data) { listTransactions(data); });
+	$.post("/transactions/new", transaction);
 
 }
